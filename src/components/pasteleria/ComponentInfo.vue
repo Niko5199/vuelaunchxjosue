e8ccbe5c5f81d388fac5bb88b226b0cdb6f18c97
Arturo Rojas <paredesjosue77@gmail.com>
2022-03-24 16:24:15 -0600

    Nuevos archivos

---
+++ src/components/pasteleria/ComponentInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="info">
+    <h3>Dirección: Av El resurgir, No 7234 Col Alamos</h3>
+    <h3>Telefono: 21932403</h3>
+    <h3>Horario de Atencion a cliente: Lun - Sab / 10am a 20pm</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentInfo',
+};
+</script>
+
+<style scoped>
+.info {
+  /* display: flex; */
+  justify-content: center;
+}
+</style>
---
+++ src/views/Pasteleria.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cotainer">
+    <ComponentInfo />
+  </div>
+</template>
+
+<script>
+import ComponentInfo from '@/components/pasteleria/ComponentInfo.vue';
+
+export default {
+  name: 'Pasteleria',
+  components: {
+    ComponentInfo,
+  },
+};
+</script>
